b2ace7f41aef81283a5afff3dd5e35e271a97bc7
youthOfMe <2040690180@qq.com>
2024-05-13 20:09:45 +0800

    feat: 测试windicss的使用

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,13 @@
 </script>
 
 <template>
-  <button class="bg-purple-500 text-indigo-50 px-4 py-2">按钮</button>
+  <button class="btn">按钮</button>
 </template>
 
 <style scoped>
+.btn {
+  @apply bg-purple-500 text-indigo-50 px-4 py-2 rounded-full 
+  transition-all duration-500 hover:(bg-purple-900) focus:(ring-8 ring-purple-900);
+}
 </style>
+ 
