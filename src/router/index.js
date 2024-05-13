38e0043c48fe7ec7853ede37b193dc570e9657d6
youthOfMe <2040690180@qq.com>
2024-05-13 20:43:35 +0800

    feat: 配置路由 配置404界面

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,25 @@ import {
   createWebHistory
 } from 'vue-router'
 
-const routes = []
+import Index from '@/pages/index.vue'
+import About from '../pages/about.vue'
+import NotFound from '@/pages/404.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: Index
+  },
+  {
+    path: '/about',
+    component: About
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(),
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCss from 'vite-plugin-windicss'
+
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  },
   plugins: [vue(), WindiCss()],
 })
