2851f7a13883dd0693dad1b606fd0ae48325d86b
youthOfMe <2040690180@qq.com>
2024-05-13 22:09:20 +0800

    feat: 提取windicss属性到@apply指令中

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,18 +1,17 @@
 <template>
-  <el-row style="min-height: 100vh;" class="bg-indigo-500">
-    <el-col :lg="16" :md="12" class="flex items-center justify-center flex-col">
+  <el-row style="min-height: 100vh;" class="login-container">
+    <el-col :lg="16" :md="12" class="left">
       <div>
-        <div class="font-bold text-5xl text-light-50 mb-4">你真牛逼</div>
-        <div class="text-gray-200 text-sm">我牛逼 你牛逼 大家都牛逼 付方亮最牛逼 赵宇轩更牛逼 牛逼牛逼很牛逼</div>
+        <div>你真牛逼</div>
+        <div>我牛逼 你牛逼 大家都牛逼 付方亮最牛逼 赵宇轩更牛逼 牛逼牛逼很牛逼</div>
       </div>
     </el-col>
-    <el-col :lg="8" :md="12" class="bg-light-50 flex items-center justify-center flex-col">
-      <h2 class="font-bold text-3xl text-gray-800">欢迎回来</h2>
-      <div class="flex items-center justify-center my-5 text-gray-300
-      space-x-2">
-        <span class="h-[1px] w-16 bg-gray-200"></span>
+    <el-col :lg="8" :md="12" class="right">
+      <h2 class="title">欢迎回来</h2>
+      <div>
+        <span class="line"></span>
         <span>账号密码登录</span>
-        <span class="h-[1px] w-16 bg-gray-200"></span>
+        <span class="line"></span>
       </div>
       <el-form :model="form" class="w-[250px]">
         <el-form-item>
@@ -50,3 +49,30 @@ const onSubmit = () => {
   console.log('submit!')
 }
 </script>
+
+<style scoped>
+.login-container {
+  @apply bg-indigo-500;
+}
+.login-container .left, .login-container .right {
+  @apply flex items-center justify-center flex-col;
+}
+.login-container .right {
+  @apply bg-light-50 flex-col ;
+}
+.left>div>div:first-child {
+  @apply font-bold text-5xl text-light-50 mb-4;
+}
+.left>div>div:last-child {
+  @apply text-gray-200 text-sm;
+}
+.right .title {
+  @apply font-bold text-3xl text-gray-800;
+}
+.right>div {
+  @apply flex items-center justify-center my-5 text-gray-300 space-x-2;
+}
+.right .line {
+  @apply h-[1px] w-16 bg-gray-200;
+}
+</style>
