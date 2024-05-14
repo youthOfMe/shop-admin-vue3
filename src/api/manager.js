fa150e4f33bbb22f6cd4419d3a911ba0b7807436
youthOfMe <2040690180@qq.com>
2024-05-14 09:40:17 +0800

    feat: 完善登录功能 封装请求/响应拦截器

--- src/api/manager.js
+++ src/api/manager.js
@@ -6,3 +6,7 @@ export function login(username, password) {
     password
   })
 }
+
+export function getInfo() {
+  return axios.post('/admin/getInfo')
+}
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -17,19 +17,24 @@
         <el-form-item prop="username">
           <el-input v-model="form.username" placeholder="请输入用户名">
             <template #prefix>
-              <el-icon class="el-input__icon"><user /></el-icon>
+              <el-icon class="el-input__icon">
+                <user />
+              </el-icon>
             </template>
           </el-input>
         </el-form-item>
         <el-form-item prop="password">
           <el-input v-model="form.password" placeholder="请输入密码" type="password" show-password>
             <template #prefix>
-              <el-icon class="el-input__icon"><lock /></el-icon>
+              <el-icon class="el-input__icon">
+                <lock />
+              </el-icon>
             </template>
-          </el-input>   
+          </el-input>
         </el-form-item>
         <el-form-item>
-          <el-button round color="#626aef" class="w-[250px]" type="primary" @click="onSubmit">登录</el-button>
+          <el-button round color="#626aef" class="w-[250px]" type="primary" @click="onSubmit"
+            :loading="loading">登录</el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -39,10 +44,9 @@
 <script lang="ts" setup>
 import { reactive, ref } from 'vue'
 import { useRouter } from 'vue-router';
-import { login } from '@/api/manager'
+import { login, getInfo } from '@/api/manager'
 import { ElNotification } from 'element-plus'
 import { useCookies } from '@vueuse/integrations/useCookies'
-const cookie = useCookies()
 
 const router = useRouter()
 
@@ -61,8 +65,8 @@ const rules = {
       trigger: 'blur'
     },
   ],
-  password: [ 
-  {
+  password: [
+    {
       required: true,
       message: '密码不能为空',
       trigger: 'blur'
@@ -71,15 +75,14 @@ const rules = {
 }
 
 const formRef = ref(null)
-
+const loading = ref(false)
 const onSubmit = () => {
   formRef.value?.validate((valid) => {
     if (!valid) {
       return false
     }
+    loading.value = true
     login(form.username, form.password).then(res => {
-      console.log(res)
-
       // 提示成功
       ElNotification({
         message: "登录成功",
@@ -89,16 +92,18 @@ const onSubmit = () => {
 
       // 存储token和用户相关信息
       const cookie = useCookies()
-      cookie.set("admin-token", res.data.data.token)
+      cookie.set("admin-token", res.token)
+
+      // 获取用户相关信息
+      getInfo().then(res2 => {
+        console.log(res2);
+
+      })
 
       // 跳转到后台首页
       router.push('/')
-    }).catch(err => {
-      ElNotification({
-        message: err.response.data.msg || "请求失败",
-        type: 'error',
-        duration: 3000
-      })
+    }).finally(() => {
+      loading.value = false
     })
   })
 }
@@ -108,24 +113,32 @@ const onSubmit = () => {
 .login-container {
   @apply bg-indigo-500;
 }
-.login-container .left, .login-container .right {
+
+.login-container .left,
+.login-container .right {
   @apply flex items-center justify-center flex-col;
 }
+
 .login-container .right {
-  @apply bg-light-50 flex-col ;
+  @apply bg-light-50 flex-col;
 }
+
 .left>div>div:first-child {
   @apply font-bold text-5xl text-light-50 mb-4;
 }
+
 .left>div>div:last-child {
   @apply text-gray-200 text-sm;
 }
+
 .right .title {
   @apply font-bold text-3xl text-gray-800;
 }
+
 .right>div {
   @apply flex items-center justify-center my-5 text-gray-300 space-x-2;
 }
+
 .right .line {
   @apply h-[1px] w-16 bg-gray-200;
 }
