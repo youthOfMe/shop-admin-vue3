e15ed9e37b413ce947b82fb20a68b625d7b538a2
youthOfMe <2040690180@qq.com>
2024-05-15 23:22:58 +0800

    feat: 统计面板组件骨架屏开发

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,27 +1,52 @@
 <template>
   <div>
     <el-row :gutter="20">
-      <el-col :span="6" :offset="0" v-for="(item, index) in panels" :key="index">
-        <el-card shadow="hover" :body-style="{ padding: '20px' }">
-          <template #header>
-            <div class="flex justify-between">
-              <span class="text-sm">{{ item.title }}</span>
-              <el-tag :type="item.unitColor || 'success'" effect="plain">
-                {{ item.unit }}
-              </el-tag>
-            </div>
-          </template>
-          <span class="text-3xl font-bold text-gray-500">
-            {{ item.value }}
-          </span>
-          <el-divider />
-          <div class="flex justify-between text-sm text-gray-500">
-            <span>{{ item.subTitle }}</span>
-            <span>{{ item.subValue }}</span>
-          </div>
-        </el-card>
+      <template v-if="panels.length === 0">
+        <el-col :span="6" v-for="i in 4">
+          <el-skeleton style="width: 100%" animated loading>
+            <template #template>
+              <el-card shadow="hover" :body-style="{ padding: '20px' }">
+                <template #header>
+                  <div class="flex justify-between">
+                    <el-skeleton-item variant="text" style="width: 50%" />
+                    <el-skeleton-item variant="text" style="width: 10%" />
+                  </div>
+                </template>
+                <el-skeleton-item variant="h3" style="width: 80%" />
+                <el-divider />
+                <div class="flex justify-between text-sm text-gray-500">
+                  <el-skeleton-item variant="text" style="width: 50%" />
+                  <el-skeleton-item variant="text" style="width: 10%" />
+                </div>
+              </el-card>
+              <el-skeleton-item variant="text" style="width: 30%" />
+            </template>
+          </el-skeleton>
+        </el-col>
+      </template>
 
-      </el-col>
+      <template v-if="panels.length !== 0">
+        <el-col :span="6" :offset="0" v-for="(item, index) in panels" :key="index">
+          <el-card shadow="hover" :body-style="{ padding: '20px' }">
+            <template #header>
+              <div class="flex justify-between">
+                <span class="text-sm">{{ item.title }}</span>
+                <el-tag :type="item.unitColor || 'success'" effect="plain">
+                  {{ item.unit }}
+                </el-tag>
+              </div>
+            </template>
+            <span class="text-3xl font-bold text-gray-500">
+              {{ item.value }}
+            </span>
+            <el-divider />
+            <div class="flex justify-between text-sm text-gray-500">
+              <span>{{ item.subTitle }}</span>
+              <span>{{ item.subValue }}</span>
+            </div>
+          </el-card>
+        </el-col>
+      </template>
     </el-row>
   </div>
 </template>
