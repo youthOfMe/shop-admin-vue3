ef765b459e36446e8b5dbe9bc7c4da0120c326fd
youthOfMe <2040690180@qq.com>
2024-05-15 22:02:07 +0800

    feat：完善标签组件

---
+++ src/composables/useTabList.js
@@ -0,0 +1,98 @@
+import { ref } from 'vue'
+import { useRoute, useRouter, onBeforeRouteUpdate } from 'vue-router'
+import { useCookies } from '@vueuse/integrations/useCookies'
+
+export function useTabList() {
+  const route = useRoute()
+  const router = useRouter()
+
+  const cookie = useCookies()
+
+  const activeTab = ref(route.path)
+  const tabList = ref([
+    {
+      title: '后台首页',
+      path: '/'
+    },
+    {
+      title: '商城管理',
+      path: '/goods/list'
+    }
+  ])
+
+  // 添加标签导航
+  const addTab = (tab) => {
+    const noTab = tabList.value.findIndex(t => t.path === tab.path) === -1
+    if (noTab) {
+      tabList.value.push(tab)
+    }
+
+    cookie.set("tabList", tabList.value)
+  }
+
+  // 初始化标签导航列表
+  const initTabList = () => {
+    const tbs = cookie.get("tabList")
+    if (tbs) {
+      tabList.value = tbs
+    }
+  }
+
+  initTabList()
+
+  onBeforeRouteUpdate((to, from) => {
+    activeTab.value = to.path
+    addTab({
+      title: to.meta.title,
+      path: to.path
+    })
+  })
+
+  const changeTab = (t) => {
+    activeTab.value = t.props.name
+    router.push(t.props.name)
+  }
+
+  const removeTab = (t) => {
+    const tabs = tabList.value
+    let a = activeTab.value
+    if (a === t) {
+      tabs.forEach((tab, index) => {
+        if (tab.path === t) {
+          const nextTab = tabs[index + 1] || tabs[index - 1]
+          if (nextTab) {
+            a = nextTab.path
+          }
+        }
+      })
+    }
+
+    activeTab.value = a
+    tabList.value = tabList.value.filter(tab => tab.path != t)
+  }
+
+  // 关闭
+  const handleClose = (command) => {
+    if (command === "clearAll") {
+      // 切换回首页
+      activeTab.value = "/"
+      // 过滤只剩下首页
+      tabList.value = [{
+        title: '后台首页',
+        path: '/'
+      },]
+    } else {
+      // 过滤只剩下首页和当前激活的
+      tabList.value = tabList.value.filter(tab => tab.path === "/" || tab.path === activeTab.value)
+    }
+    cookie.set("tabList", tabList.value)
+  }
+
+  return {
+    activeTab,
+    tabList,
+    changeTab,
+    removeTab,
+    handleClose
+  }
+}
--- src/layouts/components/FTagList.vue
+++ src/layouts/components/FTagList.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="f-tag-list" :style="{ left: $store.state.asideWidth }">
-    <el-tabs v-model="editableTabsValue" type="card" class="demo-tabs" closable @tab-remove="removeTab"
-      style="min-width: 100px;">
-      <el-tab-pane v-for="item in editableTabs" :key="item.name" :label="item.title" :name="item.name">
+    <el-tabs v-model="activeTab" type="card" style="min-width: 100px;" @tab-click="changeTab" @tab-remove="removeTab">
+      <el-tab-pane v-for="item in tabList" :key="item.path" :label="item.title" :name="item.path"
+        :closable="item.path != '/'">
       </el-tab-pane>
     </el-tabs>
 
     <span class="tag-btn">
-      <el-dropdown>
+      <el-dropdown @command="handleClose">
         <span class="el-dropdown-link">
           <el-icon class="el-icon--right">
             <arrow-down />
@@ -15,112 +15,26 @@
         </span>
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item>Action 1</el-dropdown-item>
-            <el-dropdown-item>Action 2</el-dropdown-item>
-            <el-dropdown-item>Action 3</el-dropdown-item>
-            <el-dropdown-item disabled>Action 4</el-dropdown-item>
-            <el-dropdown-item divided>Action 5</el-dropdown-item>
+            <el-dropdown-item command="clearOther">关闭其他</el-dropdown-item>
+            <el-dropdown-item command="clearAll">全部关闭</el-dropdown-item>
           </el-dropdown-menu>
         </template>
       </el-dropdown>
     </span>
   </div>
+
+  <div style="height: 44px;"></div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
-let tabIndex = 2
-const editableTabsValue = ref('2')
-const editableTabs = ref([
-  {
-    title: 'Tab 1',
-    name: '1',
-    content: 'Tab 1 content',
-  },
-  {
-    title: 'Tab 2',
-    name: '2',
-    content: 'Tab 2 content',
-  },
-  {
-    title: 'Tab 1',
-    name: '1',
-    content: 'Tab 1 content',
-  },
-  {
-    title: 'Tab 2',
-    name: '2',
-    content: 'Tab 2 content',
-  },
-  {
-    title: 'Tab 1',
-    name: '1',
-    content: 'Tab 1 content',
-  },
-  {
-    title: 'Tab 2',
-    name: '2',
-    content: 'Tab 2 content',
-  },
-  {
-    title: 'Tab 1',
-    name: '1',
-    content: 'Tab 1 content',
-  },
-  {
-    title: 'Tab 2',
-    name: '2',
-    content: 'Tab 2 content',
-  },
-  {
-    title: 'Tab 1',
-    name: '1',
-    content: 'Tab 1 content',
-  },
-  {
-    title: 'Tab 2',
-    name: '2',
-    content: 'Tab 2 content',
-  },
-  {
-    title: 'Tab 1',
-    name: '1',
-    content: 'Tab 1 content',
-  },
-  {
-    title: 'Tab 2',
-    name: '2',
-    content: 'Tab 2 content',
-  },
-])
-
-const addTab = (targetName) => {
-  const newTabName = `${++tabIndex}`
-  editableTabs.value.push({
-    title: 'New Tab',
-    name: newTabName,
-    content: 'New Tab content',
-  })
-  editableTabsValue.value = newTabName
-}
-const removeTab = (targetName) => {
-  const tabs = editableTabs.value
-  let activeName = editableTabsValue.value
-  if (activeName === targetName) {
-    tabs.forEach((tab, index) => {
-      if (tab.name === targetName) {
-        const nextTab = tabs[index + 1] || tabs[index - 1]
-        if (nextTab) {
-          activeName = nextTab.name
-        }
-      }
-    })
-  }
-
-  editableTabsValue.value = activeName
-  editableTabs.value = tabs.filter((tab) => tab.name !== targetName)
-}
+import { useTabList } from '@/composables/useTabList'
+const {
+  activeTab,
+  tabList,
+  changeTab,
+  removeTab,
+  handleClose
+} = useTabList()
 </script>
 
 <style scoped>
