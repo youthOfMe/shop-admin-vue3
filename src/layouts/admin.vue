a88f3c7baefd485713a2b4930f7fc2d96d507ea5
youthOfMe <2040690180@qq.com>
2024-05-15 22:41:20 +0800

    feat: 完善页面切换动画效果

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -10,9 +10,11 @@
       <el-main>
         <f-tag-list></f-tag-list>
         <router-view v-slot="{ Component }">
-          <keep-alive :max="10">
-            <component :is="Component"></component>
-          </keep-alive>
+          <transition name="fade">
+            <keep-alive :max="10">
+              <component :is="Component"></component>
+            </keep-alive>
+          </transition>
         </router-view>
       </el-main>
     </el-container>
@@ -25,8 +27,39 @@ import FMenu from './components/FMenu.vue'
 import FTagList from './components/FTagList.vue'
 </script>
 
-<style>
+<style scoped>
 .el-aside {
   transition: all 1s;
+}
+
+.fade-enter-from {
+  opacity: 0;
+  --webkit-transform: translate3d(0, -100%, 0);
+  transform: translate3d(0, -100%, 0);
+}
+
+.fade-enter-to {
+  opacity: 1;
+  --webkit-transform: translate3d(0, 0, 0);
+  transform: translate3d(0, 0, 0);
+}
+
+.fade-leave-from {
+  opacity: 1;
+}
+
+.fade-leave-to {
+  opacity: 0;
+  --webkit-transform: translate3d(0, 100%, 0);
+  transform: translate3d(0, 100%, 0);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s;
+}
+
+.fade-enter-active {
+  transition-delay: 0.3s;
 }
 </style>
