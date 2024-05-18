40937a5b60b1366ce2b22a4891ec8d0310b5261d
youthOfMe <2040690180@qq.com>
2024-05-18 23:43:37 +0800

    feat: 实现创建公告

--- src/pages/notice/list.vue
+++ src/pages/notice/list.vue
@@ -2,7 +2,7 @@
   <el-card shadow="never" class="border-0">
     <!-- 新增|刷新 -->
     <div class="flex items-center justify-between mb-4">
-      <el-button type="primary" size="small">新增</el-button>
+      <el-button type="primary" size="small" @click="handleCreate">新增</el-button>
       <el-tooltip effect="dark" content="刷新数据" placement="top">
         <el-button text @click="getData()">
           <el-icon :size="20">
@@ -18,8 +18,7 @@
       <el-table-column label="操作">
         <template #default="scope">
           <el-button type="primary" size="small" text>修改</el-button>
-          <el-popconfirm title="是否要删除该公告?" confirmButtonText="确认" cancelButtonText="取消"
-            @confirm="handleDelete(item.id)">
+          <el-popconfirm title="是否要删除该公告?" confirmButtonText="确认" cancelButtonText="取消" @confirm="handleDelete()">
             <template #reference>
               <el-button type="primary" size="small" text>删除</el-button>
             </template>
@@ -33,15 +32,33 @@
       <el-pagination layout="prev, pager, next" background :total="total" :current-page="currentPage" :page-size="limit"
         @current-change="getData" />
     </div>
+
+    <FormDrawer ref="formDrawerRef" title="新增" @submit="handleSubmit">
+      <el-form :model="form" ref="formRef" :rules="rules" label-width="80px" :inline="false">
+        <el-form-item label="公告标题" prop="title">
+          <el-input v-model="form.title" placeholder="公告标题"></el-input>
+        </el-form-item>
+        <el-form-item label="公告内容" prop="content">
+          <el-input v-model="form.content" placeholder="公告内容" type="textarea" :rows="5"></el-input>
+        </el-form-item>
+      </el-form>
+
+    </FormDrawer>
+
   </el-card>
 
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
+import FormDrawer from '@/components/FormDrawer.vue'
 import {
-  getNoticeList
+  getNoticeList,
+  createNotice
 } from '@/api/notice'
+import {
+  toast
+} from '@/composables/util'
 
 const tableData = ref([])
 
@@ -57,12 +74,55 @@ const getData = (p) => {
   getNoticeList(currentPage.value).then(res => {
     tableData.value = res.list
     total.value = res.totalCount
-    if (item) {
-      handleChangeActiveId(item.id)
-    }
   }).finally(() => {
   })
 }
-
 getData()
+
+// 发布公告
+// 表单部分
+const formDrawerRef = ref(null)
+const formRef = ref(null)
+const form = reactive({
+  title: ''
+  content: ''
+})
+const rules = {
+  title: [
+    {
+      required: true,
+      message: '公告标题不能为空',
+      trigger: 'blur'
+    }
+  ],
+  content: [
+    {
+      required: true,
+      message: '公告内容不能为空',
+      trigger: 'blur'
+    }
+  ]
+}
+
+const handleSubmit = () => {
+  formRef.value.validate((valid) => {
+    if (!valid) return
+
+    formDrawerRef.value.showLoading()
+
+    createNotice(form).then(res => {
+      toast("新增成功")
+      getData(1)
+      formDrawerRef.value.close()
+    }).finally(() => {
+      formDrawerRef.value.hideLoading()
+    })
+  })
+}
+
+// 新增
+const handleCreate = () => {
+  formDrawerRef.value.open()
+}
+
 </script>
