dae76a4f4cf1009f0cf713690e4e7e5c35b432fe
youthOfMe <2040690180@qq.com>
2024-05-20 13:52:55 +0800

    feat: 搭建基本的权限树状架构

--- src/pages/access/list.vue
+++ src/pages/access/list.vue
@@ -1,5 +1,68 @@
 <template>
   <div>
-    菜单权限
+    <el-card shadow="never" class="border-0">
+      <!-- 新增|刷新 -->
+      <ListHeader @refresh="getData"></ListHeader>
+      <el-tree v-loading="loading" :data="tableData" :props="{ label: 'name', children: 'child' }" node-key="id"
+        :default-expanded-keys="defaultExpandedKeys">
+        <template #default="{ node, data }">
+          <div class="custom-tree-node">
+            <el-tag size="small" :type="data.menu ? '' : 'info'">{{ data.menu ? '菜单' : '权限' }}</el-tag>
+            <el-icon v-if="data.icon" :size="16" class="ml-2">
+              <component :is="data.icon"></component>
+            </el-icon>
+            <span>{{ data.name }}</span>
+
+            <div class="ml-auto">
+              <el-switch :modelValue="status" :active-value="1" :inactive-value="0">
+              </el-switch>
+              <el-button text type="primary" size="small">修改</el-button>
+              <el-button text type="primary" size="small">增加</el-button>
+              <el-button text type="primary" size="small">删除</el-button>
+            </div>
+          </div>
+        </template>
+      </el-tree>
+    </el-card>
+
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import ListHeader from '@/components/ListHeader.vue'
+import {
+  getRuleList
+} from '@/api/rule'
+
+import {
+  useInitTable
+} from '@/composables/useCommon'
+
+const defaultExpandedKeys = ref([])
+const {
+  loading,
+  tableData,
+  getData
+} = useInitTable({
+  getList: getRuleList,
+  onGetListSuccess: (res) => {
+    tableData.value = res.list
+    defaultExpandedKeys.value = res.list.map(o => o.id)
+  }
+})
+</script>
+
+<style>
+.custom-tree-node {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  padding-right: 8px;
+}
+
+.el-tree-node__content {
+  padding: 20px;
+}
+</style>
--- src/pages/manager/list.vue
+++ src/pages/manager/list.vue
@@ -17,18 +17,8 @@
       </el-row>
     </el-form>
 
-
     <!-- 新增|刷新 -->
-    <div class="flex items-center justify-between mb-4">
-      <el-button type="primary" size="small" @click="handleCreate">新增</el-button>
-      <el-tooltip effect="dark" content="刷新数据" placement="top">
-        <el-button text @click="getData()">
-          <el-icon :size="20">
-            <Refresh></Refresh>
-          </el-icon>
-        </el-button>
-      </el-tooltip>
-    </div>
+    <ListHeader @create="handleCreate" @refresh="getData"></ListHeader>
 
     <el-table :data="tableData" stripe style="width: 100%" v-loading="loading">
       <el-table-column label="管理员">
@@ -108,6 +98,7 @@
 <script setup>
 import { ref } from 'vue'
 import FormDrawer from '@/components/FormDrawer.vue'
+import ListHeader from '@/components/ListHeader.vue'
 import ChooseImage from '@/components/ChooseImage.vue'
 
 import {
@@ -121,8 +112,6 @@ import {
   useInitTable,
   useInitForm
 } from '@/composables/useCommon'
-
-console.log(useInitForm);
 
 // 权限列表
 const roles = ref([])
