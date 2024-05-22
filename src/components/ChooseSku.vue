5863ed45f66e82e9629787df8d99fffab0ff384a
youthOfMe <2040690180@qq.com>
2024-05-22 22:33:20 +0800

    feat: 商品管理-实现规格的选择

--- src/components/ChooseSku.vue
+++ src/components/ChooseSku.vue
@@ -4,7 +4,10 @@
     <el-container style="height: 65vh;">
       <el-aside width="220px" class="image-aside">
         <div class="top">
-          列表部分
+          <div class="sku-list" :class="{ 'active': activeId === item.id }" v-for="(item, index) in tableData"
+            :key="index" @click="handleChangeActiveId(item.id)">
+            {{ item.name }}
+          </div>
         </div>
         <div class="bottom">
           <el-pagination layout="prev, next" background :total="total" :current-page="currentPage" :page-size="limit"
@@ -12,7 +15,12 @@
         </div>
       </el-aside>
       <el-main>
-        内容
+        <el-checkbox-group v-model="form.list">
+          <el-checkbox v-for="item in list" :key="item" :label="item" border>
+            {{ item }}
+          </el-checkbox>
+        </el-checkbox-group>
+
       </el-main>
     </el-container>
 
@@ -27,7 +35,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 import {
   getSkusList
 } from '@/api/skus'
@@ -36,6 +44,7 @@ import {
 } from '@/composables/useCommon'
 
 const dialogVisible = ref(false)
+const activeId = ref(0)
 
 const {
   loading,
@@ -45,12 +54,35 @@ const {
   tableData,
   getData
 } = useInitTable({
-  getList: getSkusList
+  getList: getSkusList,
+  onGetListSuccess: (res) => {
+    tableData.value = res.list
+    total.value = res.totalCount
+
+    if (tableData.value.length > 0) {
+      handleChangeActiveId(tableData.value[0].id)
+    }
+  }
 })
 
 const open = () => {
   getData(1)
   dialogVisible.value = true
+}
+
+const list = ref([])
+
+const form = reactive({
+  list: []
+})
+
+function handleChangeActiveId(id) {
+  activeId.value = id
+  list.value = []
+  const item = tableData.value.find(o => o.id === id)
+  if (item) {
+    list.value = item.default.split(",")
+  }
 }
 
 // 提交
@@ -85,5 +117,15 @@ defineExpose({
   left: 0;
   right: 0;
   @apply flex items-center justify-center;
+}
+
+.sku-list {
+  border-bottom: 1px solid #f4f4f4;
+  @apply p-3 text-sm text-gray-600 flex items-center cursor-pointer;
+}
+
+.sku-list:hover,
+.active {
+  @apply bg-blue-50;
 }
 </style>
