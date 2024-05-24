283eb801cfdef58851db0f0555db0a7962ee1d8e
youthOfMe <2040690180@qq.com>
2024-05-24 10:31:48 +0800

    feat: 用户管理-会员管理-完成新增会员 完善所有接口对接

--- src/pages/level/list.vue
+++ src/pages/level/list.vue
@@ -4,8 +4,9 @@
     <ListHeader @create="handleCreate" @refresh="getData"></ListHeader>
 
     <el-table :data="tableData" stripe style="width: 100%" v-loading="loading">
-      <el-table-column prop="name" label="角色名称" />
-      <el-table-column prop="desc" label="角色描述" width="380" />
+      <el-table-column prop="name" label="会员等级" />
+      <el-table-column prop="discount" label="折扣率" align="center" />
+      <el-table-column prop="level" label="等级序号" />
       <el-table-column label="状态" width="120">
         <template #default="{ row }">
           <el-switch :modelValue="row.status" :active-value="1" :inactive-value="0"
@@ -16,7 +17,7 @@
       <el-table-column label="操作">
         <template #default="scope">
           <el-button type="primary" size="small" text @click="handleEdit(scope.row)">修改</el-button>
-          <el-popconfirm title="是否要删除该公告?" confirmButtonText="确认" cancelButtonText="取消"
+          <el-popconfirm title="是否要删除该记录?" confirmButtonText="确认" cancelButtonText="取消"
             @confirm="handleDelete(scope.row.id)">
             <template #reference>
               <el-button type="primary" size="small" text>删除</el-button>
@@ -33,15 +34,43 @@
 
     <FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit">
       <el-form :model="form" ref="formRef" :rules="rules" label-width="80px" :inline="false">
-        <el-form-item label="角色名称" prop="name">
-          <el-input v-model="form.name" placeholder="角色名称"></el-input>
+        <el-form-item label="等级名称" prop="name">
+          <el-input v-model="form.name" placeholder="等级名称"></el-input>
         </el-form-item>
-        <el-form-item label="角色描述" prop="desc">
-          <el-input v-model="form.desc" placeholder="角色描述" type="textarea" :rows="5"></el-input>
+        <el-form-item label="等级权重" prop="level">
+          <el-input type="number" v-model="form.level" placeholder="等级权重"></el-input>
         </el-form-item>
         <el-form-item label="状态" prop="desc">
           <el-switch v-model="form.status" :active-value="1" :inactive-value="0">
           </el-switch>
+        </el-form-item>
+        <el-form-item label="升级条件">
+          <div>
+            <small class="text-xs mr-2">累计消费满</small>
+            <el-input type="number" v-model="form.max_price" placeholder="累计消费" style="width: 50%;">
+              <template #append>元</template>
+            </el-input>
+            <small class="text-gray-400 flex">
+              设置会员等级所需要的累计消费必须大于等于0, 单位: 元
+            </small>
+          </div>
+          <div>
+            <small class="text-xs mr-2">累计次数满</small>
+            <el-input type="number" v-model="form.max_times" placeholder="累计次数" style="width: 50%;">
+              <template #append>元</template>
+            </el-input>
+            <small class="text-gray-400 flex">
+              设置会员等级所需要的购买量必须大于等于0, 单位: 笔
+            </small>
+          </div>
+        </el-form-item>
+        <el-form-item label="折扣率(%)" prop="discount">
+          <el-input type="number" v-model="form.discount" placeholder="折扣率(%)" style="width: 50%;">
+            <template #append>%</template>
+          </el-input>
+          <small class="text-gray-400 flex">
+            折扣率单位为百分比, 如输入90, 表示该会员等级的用户可以以商品原价的90%购买
+          </small>
         </el-form-item>
       </el-form>
 
@@ -60,7 +89,7 @@ import {
   updateUserLevel,
   deleteUserLevel,
   updateUserLevelStatus,
-} from '@/api/role'
+} from '@/api/level'
 
 import {
   useInitTable,
@@ -95,14 +124,18 @@ const {
 } = useInitForm({
   form: {
     name: '',
-    desc: '',
-    status: 1
+    level: 100,
+    status: 1,
+    discount: 0,
+    max_price: 0,
+    min_price: 0,
+    max_times: 0
   },
   rules: {
     name: [
       {
         required: true,
-        message: '角色名称不能为空',
+        message: '会员等级名称不能为空',
         trigger: 'blur'
       }
     ],
