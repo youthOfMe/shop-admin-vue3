a0c027be15df3bbf87cf9b547d594158f96672e2
youthOfMe <2040690180@qq.com>
2024-05-24 10:45:52 +0800

    feat: 搭建用户管理的基础架构

--- src/pages/user/list.vue
+++ src/pages/user/list.vue
@@ -1,5 +1,169 @@
 <template>
-  <div>
-    用户列表
-  </div>
+  <el-card shadow="never" class="border-0">
+    <!-- 搜索 -->
+    <Search @Search="getData" @reset="resetSearchForm" :model="searchForm">
+      <SearchItem label="关键词">
+        <el-input v-model="searchForm.keyword" placeholder="手机号/邮箱/会员昵称" clearable></el-input>
+      </SearchItem>
+      <template #show>
+        <SearchItem label="会员等级">
+          <el-select v-model="searchForm.user_level_id" placeholder="请选择会员等级" clearable>
+            <el-option v-for="item in user_level" :key="item.id" :label="item.name" :value="item.id">
+            </el-option>
+          </el-select>
+
+        </SearchItem>
+      </template>
+    </Search>
+
+    <!-- 新增|刷新 -->
+    <ListHeader @create="handleCreate" @refresh="getData"></ListHeader>
+
+    <el-table :data="tableData" stripe style="width: 100%" v-loading="loading">
+      <el-table-column label="管理员">
+        <template #default="{ row }">
+          <div class="flex items-center">
+            <el-avatar :size="40" :src="row.avatar">
+              <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png" />
+            </el-avatar>
+            <div class="ml-3">
+              <h6>{{ row.username }}</h6>
+              <small>ID: {{ row.id }}</small>
+            </div>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column label="所属管理员" align="center">
+        <template #default="{ row }">
+          {{ row.role?.name || '-' }}
+        </template>
+      </el-table-column>
+      <el-table-column label="状态" width="120">
+        <template #default="{ row }">
+          <el-switch :modelValue="row.status" :active-value="1" :inactive-value="0"
+            @change="handleStatusChange($event, row)" :loading="row.statusLoading" :disabled="row.super === 1">
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template #default="scope">
+          <small v-if="scope.row.super === 1" class="text-sm text-gray-500">暂无操作</small>
+          <div v-else>
+            <el-button type="primary" size="small" text @click="handleEdit(scope.row)">修改</el-button>
+            <el-popconfirm title="是否要删除该管理员?" confirmButtonText="确认" cancelButtonText="取消"
+              @confirm="handleDelete(scope.row.id)">
+              <template #reference>
+                <el-button type="primary" size="small" text>删除</el-button>
+              </template>
+            </el-popconfirm>
+          </div>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div class="flex items-center justify-center mt-5">
+      <el-pagination layout="prev, pager, next" background :total="total" :current-page="currentPage" :page-size="limit"
+        @current-change="getData" />
+    </div>
+
+    <FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit">
+      <el-form :model="form" ref="formRef" label-width="80px" :inline="false">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="form.username" placeholder="用户名"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="form.password" placeholder="密码"></el-input>
+        </el-form-item>
+        <el-form-item label="头像" prop="avatar">
+          <ChooseImage v-model="form.avatar" />
+        </el-form-item>
+        <el-form-item label="所属角色" prop="role_id">
+          <el-select v-model="form.role_id" placeholder="选择所属角色">
+            <el-option v-for="item in roles" :key="item.id" :label="item.name" :value="item.id">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="状态" prop="status">
+          <el-switch v-model="form.status" :active-value="1" :inactive-value="0">
+          </el-switch>
+        </el-form-item>
+      </el-form>
+    </FormDrawer>
+
+  </el-card>
+
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import FormDrawer from '@/components/FormDrawer.vue'
+import ListHeader from '@/components/ListHeader.vue'
+import ChooseImage from '@/components/ChooseImage.vue'
+import Search from '@/components/Search.vue'
+import SearchItem from '@/components/SearchItem.vue'
+
+import {
+  getUserList,
+  updateUserStatus,
+  createUser,
+  updateUser,
+  deleteUser
+} from '@/api/user'
+import {
+  useInitTable,
+  useInitForm
+} from '@/composables/useCommon'
+
+// 会员等级
+const user_level = ref([])
+
+const {
+  searchForm,
+  resetSearchForm,
+  tableData,
+  loading,
+  currentPage,
+  total,
+  limit,
+  getData,
+  handleDelete,
+  handleStatusChange,
+} = useInitTable({
+  searchForm: {
+    keyword: ""
+  },
+  getList: getUserList,
+  onGetListSuccess: (res) => {
+    tableData.value = res.list.map(o => {
+      o.statusLoading = false
+      return o
+    })
+    total.value = res.totalCount
+    user_level.value = res.user_level
+  },
+  delete: deleteUser,
+  updateStatus: updateUserStatus
+})
+
+const {
+  formDrawerRef,
+  formRef,
+  form,
+  rules,
+  drawerTitle,
+  handleSubmit,
+  handleCreate,
+  handleEdit
+} = useInitForm({
+  form: {
+    username: '',
+    password: '',
+    role_id: null,
+    status: 1,
+    avatar: ''
+  },
+  getData,
+  update: updateUser,
+  create: createUser
+})
+</script>
