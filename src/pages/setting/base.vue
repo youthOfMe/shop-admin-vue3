cf642519501e5f2ef97cf0055a2d4c3bb0272e77
youthOfMe <2040690180@qq.com>
2024-05-25 12:48:58 +0800

    feat: 完善基础设置保存功能

--- src/pages/setting/base.vue
+++ src/pages/setting/base.vue
@@ -14,7 +14,7 @@
             </el-radio-group>
           </el-form-item>
           <el-form-item label="注册类型">
-            <el-radio-group v-model="form.open_reg">
+            <el-radio-group v-model="form.reg_method">
               <el-radio label="username" border>
                 普通注册
               </el-radio>
@@ -28,23 +28,56 @@
           </el-form-item>
           <el-form-item label="强制密码复杂度">
             <el-checkbox-group v-model="form.password_encrypt">
-              <el-checkbox :label="0" border>数字</el-checkbox>
-              <el-checkbox :label="1" border>小写字母</el-checkbox>
-              <el-checkbox :label="2" border>大写字母</el-checkbox>
-              <el-checkbox :label="3" border>符号</el-checkbox>
+              <el-checkbox label="0" border>数字</el-checkbox>
+              <el-checkbox label="1" border>小写字母</el-checkbox>
+              <el-checkbox label="2" border>大写字母</el-checkbox>
+              <el-checkbox label="3" border>符号</el-checkbox>
             </el-checkbox-group>
           </el-form-item>
         </el-tab-pane>
         <el-tab-pane label="上传设置" name="second">
-
+          <el-form-item label="默认上传方式">
+            <el-radio-group v-model="form.upload_method">
+              <el-radio label="oss" border>
+                对象存储
+              </el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="Bucket">
+            <el-input v-model="form.upload_config.Bucket" placeholder="Bucket" style="width: 30%;"></el-input>
+          </el-form-item>
+          <el-form-item label="ACCESS_KEY">
+            <el-input v-model="form.upload_config.ACCESS_KEY" placeholder="密码最小长度" style="width: 30%;"></el-input>
+          </el-form-item>
+          <el-form-item label="SECRET_KEY">
+            <el-input v-model="form.upload_config.SECRET_KEY" placeholder="密码最小长度" style="width: 30%;"></el-input>
+          </el-form-item>
+          <el-form-item label="空间域名">
+            <el-input v-model="form.upload_config.http" placeholder="空间域名" style="width: 30%;"></el-input>
+            <small class="text-gray-500 flex mt-1">请补全 http:// 或 https://</small>
+          </el-form-item>
         </el-tab-pane>
         <el-tab-pane label="API安全" name="third">
-
+          <el-form-item label="是否开启API安全">
+            <el-radio-group v-model="form.api_safe">
+              <el-radio :label="0" border>
+                关闭
+              </el-radio>
+              <el-radio :label="1" border>
+                开启
+              </el-radio>
+            </el-radio-group>
+            <small class="text-gray-500 flex mt-1">api安全功能开启之后调用前端api需要传输签名</small>
+          </el-form-item>
+          <el-form-item label="密钥">
+            <el-input v-model="form.api_secret" placeholder="密钥" style="width: 30%;"></el-input>
+            <small
+              class="text-gray-500 flex mt-1">秘钥设置关系系统中api调用传输签名串的编码规则，以及会员token解析，请慎重设置，注意设置之后对应会员要求重新登录获取token</small>
+          </el-form-item>
         </el-tab-pane>
       </el-tabs>
       <el-form-item>
-        <el-button type="primary" size="default">保存</el-button>
-
+        <el-button type="primary" size="default" @click="submit">保存</el-button>
       </el-form-item>
 
     </el-form>
@@ -78,10 +111,27 @@ const loading = ref(false)
 function getData() {
   loading.value = true
   getSysconfig().then(res => {
-    console.log(res)
+    for (const key in form) {
+      form[key] = res[key]
+    }
+    form.password_encrypt = form.password_encrypt.split(',')
   }).finally(() => {
     loading.value = false
   })
 }
 getData()
+
+const submit = () => {
+  loading.value = true
+
+  setSysconfig({
+    ...form,
+    password_encrypt: form.password_encrypt.join(',')
+  }).then(res => {
+    toast('修改成功')
+    getData()
+  }).finally(() => {
+    loading.value = false
+  })
+}
 </script>
